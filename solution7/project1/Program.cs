a51d861b337c40de3a68f1c154f197e60593b5b2
Alham Saif <computerengalham@gmail.com>
2026-04-20 11:31:17 +0400

    solve 10 questions in (for and foreach )

---
+++ solution7/project1/Program.cs
@@ -0,0 +1,151 @@
+﻿namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region q1
+            //int[] number = {10,20,30,40,50};
+            //for (int i = 0; i < number.Length; i++)
+            //{ Console.WriteLine(number[i]); }
+
+            #endregion
+
+
+
+            #region q2
+
+            //int[] number = { 10, 20, 30, 40, 50 };
+            //foreach (int i in number)
+            // {  Console.WriteLine(i);}
+
+            #endregion
+
+
+            #region q3
+            //int[] number = new int[5];
+
+            //for (int i = 0; i < number.Length; i++)
+            //{
+            //    Console.WriteLine("Enter number:");
+            //    number[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //foreach (int i in number)
+            //{
+            //    Console.WriteLine(i); } 
+            #endregion
+
+            #region q4
+            //int[] number = new int[5];
+            //int sum = 0;
+            //for (int i = 0; i < number.Length; i++)
+            //{
+            //    Console.WriteLine("Enter number:");
+            //    number[i] = Convert.ToInt32(Console.ReadLine());
+            //    sum += number[i];
+            //}
+
+            //    Console.WriteLine("sum = "+sum); 
+            #endregion
+
+            #region q5
+            //int[] number = new int[5];
+            //int max = int.MinValue;
+            //for (int i = 0; i < number.Length; i++)
+            //{
+            //    Console.WriteLine("Enter number:");
+            //    number[i] = Convert.ToInt32(Console.ReadLine());
+            //    if (number[i] > max)
+            //    { max = number[i]; }
+            //}
+            //Console.WriteLine("max = " + max);
+
+            #endregion
+
+
+            #region q6
+            //int[] number = new int[5];
+            //int Min = int.MaxValue;
+            //for (int i = 0; i < number.Length; i++)
+            //{
+            //    Console.WriteLine("Enter number:");
+            //    number[i] = Convert.ToInt32(Console.ReadLine());
+
+
+            //    if (number[i] < Min)
+            //    { Min = number[i]; }
+            //}
+
+            //Console.WriteLine("Min = " + Min);
+
+            #endregion
+
+
+            #region q7
+            //int[] number = new int[10];
+
+            //for (int i = 0; i < number.Length; i++)
+            //{
+            //    Console.WriteLine("enter 10 numbers");
+            //    number[i] = Convert.ToInt32(Console.ReadLine());
+
+            //}
+            //Console.WriteLine("even numbers:");
+            //foreach (int i in number)
+            //{
+            //    if (i % 2 == 0) { 
+            //        Console.WriteLine(i);} }
+
+            #endregion
+
+
+            #region q8
+            //int[] number = new int[5];
+            //int sum = 0;
+            //int average = 0;
+            //for (int i = 0; i < number.Length; i++)
+            //{
+            //    Console.WriteLine("Enter number:");
+            //    number[i] = Convert.ToInt32(Console.ReadLine());
+            //    sum += number[i];
+            //    average=sum/number.Length;
+            //}
+
+            //Console.WriteLine("average = " + average);
+
+            #endregion
+
+
+            #region q9
+            //int[] number = new int[5];
+
+            //for (int i = 0; i < number.Length; i++)
+            //{
+            //    Console.WriteLine("Enter number:");
+            //    number[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.WriteLine("REVERSE:");
+            //for (int i= number.Length-1; i >=0; i--)
+            //{
+            //    Console.WriteLine(number[i]);
+            //} 
+            #endregion
+
+            #region q10
+            //int[] number = { 10, 20, 30, 40, 50 };
+            //int target = 30;
+
+            //int index = Array.IndexOf(number, target);
+
+            //if (index != -1)
+            //{
+            //    Console.WriteLine("Found at index: " + index);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not Found");
+            //} 
+            #endregion
+        }
+    }
+}
